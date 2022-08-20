7504265f201875797f90821945ed090a362a3e6e
Vinicius Wrege <viniwrege@gmail.com>
2022-08-19 22:39:39 -0300

    just saving progress

--- lista1/exec2/main.c
+++ lista1/exec2/main.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 void main(){
 
     char tempNome[16];
-    char *stringDinamica;
-    int i = 0;
+    char *stringDinamica, *pStringEnd;
+    int i = 0, j = 0;
 
-    fgets(tempNome, 16, stdin);
-    fgetche(); //limpa buffer
+    printf("entre com o nome: ");
+    scanf("%[^\n]%*c", tempNome);
 
-    while(tempNome[i] != '\0'){        
-        
-        i++;
-
-    }
+    i = strlen(tempNome);
+    tempNome[i] = ';';
+    tempNome[i+1] = '\0';
+    i = strlen(tempNome);
     
     stringDinamica = malloc(sizeof(char)*i);
+
+    strcpy(stringDinamica, tempNome);
+    
+
+    printf("entre com o nome: ");
+    scanf("%[^\n]%*c", tempNome);
+    i = strlen(tempNome);
+    tempNome[i] = ';';
+    tempNome[i+1] = '\0';
+    i = strlen(tempNome);
+
+    pStringEnd = stringDinamica;    
+    for(i = 1; *pStringEnd != '\0'; i++, pStringEnd++){
+        printf("%d", i);
+        printf("%c\n", *pStringEnd);
+
+    }    
+
+    stringDinamica = realloc(stringDinamica, sizeof(char)*i+sizeof(char)*j);
+    strcpy(stringDinamica, tempNome);
+
+    //for(i; i >= 0; stringDinamica++)
+    
 
 
 
--- lista1/readme.md
+++ lista1/readme.md
@@ -1,4 +1,4 @@
 Aqui se encontram os problemas da lista 1</br>
 link do video 1:
-https://drive.google.com/file/d/1-xly7Sn7y9feFsV5dxxHQtnJccmpYtiB/view?usp=sharing
+https://drive.google.com/file/d/1-xly7Sn7y9feFsV5dxxHQtnJccmpYtiB/view?usp=sharing </br>
 link do video 2:
