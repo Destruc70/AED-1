91c77fc9a96021b4401d864dd8dc5a8d203b55c6
Vinicius Wrege <viniwrege@gmail.com>
2022-08-19 23:56:20 -0300

    another source update

--- lista1/exec2/main.c
+++ lista1/exec2/main.c
@@ -5,37 +5,51 @@ void main(){
 
     char tempNome[16];
     char *stringDinamica, *pStringEnd;
-    int i = 0, j = 0;
+    int i = 0, j = 0, temp;
 
-    printf("entre com o nome: ");
-    scanf("%[^\n]%*c", tempNome);
-
-    i = strlen(tempNome);
-    tempNome[i] = ';';
-    tempNome[i+1] = '\0';
-    i = strlen(tempNome);
-    
-    stringDinamica = malloc(sizeof(char)*i);
-
-    strcpy(stringDinamica, tempNome);
+    stringDinamica = malloc(sizeof(char));
+    *stringDinamica = '\000';
     
 
     printf("entre com o nome: ");
     scanf("%[^\n]%*c", tempNome);
+
     i = strlen(tempNome);
     tempNome[i] = ';';
     tempNome[i+1] = '\0';
     i = strlen(tempNome);
 
     pStringEnd = stringDinamica;    
-    for(i = 1; *pStringEnd != '\0'; i++, pStringEnd++){
-        printf("%d", i);
-        printf("%c\n", *pStringEnd);
+    for(j = 0; *pStringEnd != '\0'; j++, pStringEnd++);
 
-    }    
+    if(j > 0){
 
-    stringDinamica = realloc(stringDinamica, sizeof(char)*i+sizeof(char)*j);
+        stringDinamica = realloc(stringDinamica, sizeof(char)*i+sizeof(char)*j);//realoca para tamanho da string;
+        printf("entrou");
+
+    }
+    else stringDinamica = realloc(stringDinamica, sizeof(char)*i+sizeof(char)*j);//realoca para tamanho da string;
+
     strcpy(stringDinamica, tempNome);
+    
+    for(j = 0; *pStringEnd != '\0'; j++, pStringEnd++);
+    pStringEnd = pStringEnd - j;
+
+    pStringEnd = realloc(pStringEnd, 3);//realoca para tamanho da string;
+
+    printf("entre com o nome: ");
+    scanf("%[^\n]%*c", tempNome);
+    i = strlen(tempNome);
+    tempNome[i] = ';';
+    tempNome[i+1] = '\0';
+    i = strlen(tempNome);
+
+    temp = sizeof(char)*i+sizeof(char)*j;
+    printf("aqui 22");
+    stringDinamica = realloc(stringDinamica, temp);//realoca para tamanho da string;
+
+    strcpy(stringDinamica, tempNome);
+    printf("aqui ");
 
     //for(i; i >= 0; stringDinamica++)
     
